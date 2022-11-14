867bdc309f0c5b518ec1f9f86184431b1a85dc1d
MuriloVieira <murilo@pizzamiglio.eti.br>
2022-11-14 17:16:21 -0300

    Tela movimentação criada

--- AbrirCamera.py
+++ AbrirCamera.py
@@ -2,10 +2,69 @@ import cv2 as cv #Precisa baixar
 import tkinter as tk #Precisa baixar
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk as tkk
 from PIL import Image, ImageTk #Precisa baixar
 import os
 import numpy as np #Precisa baixar provavelmente
 import face_recognition
+
+#Função tela movimentação
+def telaMov():
+    tela_principal.withdraw()
+    tela_mov = tk.Toplevel(tela_principal)
+    screen_width = tela_mov.winfo_screenwidth()
+    screen_width = (screen_width/2) - (1050/2)
+    screen_height = tela_mov.winfo_screenheight()
+    screen_height = (screen_height/2) - (500/2)  
+    tela_mov.geometry('1050x500+%d+%d' % (screen_width, screen_height)) #Tamanho
+    tela_mov.title('Movimentação')
+    webcam = tk.Label(tela_mov, relief='solid', width=450, height=350)
+    webcam.place(x=5, y=5)
+    informations = tk.Label(tela_mov,justify=tk.LEFT,bd=2,wraplength='1100',relief='solid',font=('Terminal', '16'),text="Informações:\n1) Informe nos campos a cima o seu código individual, codigo dos EPIS que deseja e ao lado a quantidade desse mesmo EPI.\n2) Tente posicionar sua face no centro do retangulo da camera deixando toda sua cabeça fique visivel.\n3) Se possivel remova óculos e bonés ou qualquer outro acessório que tampe ou esconda sua face.\n4) Por fim, olhe para a camera e clique no botão registrar ou enter no teclado sem desviar o olhar.\n5) Uma tela de confirmação aparece oficializando a movimentação casa nao haja erro")
+    informations.place(x=5, y=365)
+    codLabel = tk.Label(tela_mov, font=('System', '17', 'bold'), text='Código de funcionário:')
+    codLabel.place(x=490, y=5)
+    cod = tk.Entry(tela_mov, width=10, font=('Terminal','15'))
+    cod.place(x=790, y=15)
+    listaEpis = ['8989 - Luvas', '8787 - Capacete', '9095 - Mascara']
+    #listaEpis.append('9090 - Fone') Adicionar 1
+    #listaEpis.extend('9090 - Fone', '8765 - Creme') Adicionar mais de 1
+
+    codEpi1Label = tk.Label(tela_mov, font=('System', '17', 'bold'), text='Código do EPI:')
+    codEpi1Label.place(x=490, y=55)
+    epis1 = tkk.Combobox(tela_mov, values=listaEpis, font=('Terminal', '15'), width=30)
+    epis1.place(x=690,y=63)
+
+    codEpi2Label = tk.Label(tela_mov, font=('System', '17', 'bold'), text='Código do EPI:')
+    codEpi2Label.place(x=490, y=105)
+    epis2 = tkk.Combobox(tela_mov, values=listaEpis, font=('Terminal', '15'), width=30)
+    epis2.place(x=690,y=113)
+
+    codEpi3Label = tk.Label(tela_mov, font=('System', '17', 'bold'), text='Código do EPI:')
+    codEpi3Label.place(x=490, y=155)
+    epis3 = tkk.Combobox(tela_mov, values=listaEpis, font=('Terminal', '15'), width=30)
+    epis3.place(x=690,y=163)
+
+    codEpi4Label = tk.Label(tela_mov, font=('System', '17', 'bold'), text='Código do EPI:')
+    codEpi4Label.place(x=490, y=205)
+    epis1 = tkk.Combobox(tela_mov, values=listaEpis, font=('Terminal', '15'), width=30)
+    epis1.place(x=690,y=213)
+
+    codEpi5Label = tk.Label(tela_mov, font=('System', '17', 'bold'), text='Código do EPI:')
+    codEpi5Label.place(x=490, y=255)
+    epis5 = tkk.Combobox(tela_mov, values=listaEpis, font=('Terminal', '15'), width=30)
+    epis5.place(x=690,y=263)
+
+    registrarButton = tk.Button(tela_mov, text="Movimentar",font=('System','3'),height=2, width=15, border=10, activebackground='green')
+    registrarButton.place(x=690, y=300)
+    
+    #Acessar a camera
+    camera = cv.VideoCapture(0)
+    pegarFrames(camera,webcam)#Capturar os frames com a webcam
+
+    tela_principal.wait_window(tela_mov)
+    if(tk.Toplevel.winfo_exists(tela_mov) == 0):
+        tela_principal.deiconify()
 
 #Função tela sobre
 def telaSobre():
@@ -101,6 +160,7 @@ def registrar(cod,camera, tela_registro, name):
         
             faceFinal = cv.medianBlur(face, 1)  
             faceFinal = cv.morphologyEx(faceFinal,cv.MORPH_CLOSE, kernel, iterations=1)
+            #Cortar a imagem faceFinal = faceFinal[65:350+65 ,145:450+145]
             cv.imwrite('C:/EPIPY_CONTROL/FACES/Face_' + cod + '.jpg' , faceFinal)
             messagebox.showinfo("Sucesso", "Seu registro foi efetuado com sucesso!")
             tela_registro.destroy()
@@ -118,7 +178,7 @@ screen_height = (screen_height/2) - (500/2)
 
 tela_principal.geometry('500x500+%d+%d' % (screen_width, screen_height)) #Tamanho
 titulo=tk.Label(tela_principal, text='Controle de Entrega de EPI', font=('Terminal', '16', 'bold italic'))
-loginButton = tk.Button(tela_principal,font=('System','3'), text="Movimento", command=lambda:tela_principal.quit(), height=1, width=20, border=10)
+loginButton = tk.Button(tela_principal,font=('System','3'), text="Movimento", command=lambda:telaMov(), height=1, width=20, border=10)
 registerButton = tk.Button(tela_principal,font=('System','3') ,text="Registro", command=lambda:telaRegistro(), height=1, width=20, border=10)
 sobreButton = tk.Button(tela_principal, font=('System','3'),text="Sobre", command=lambda:telaSobre(),height=1, width=5, border=5)
 loginButton.place(x=165, y=200)
@@ -127,5 +187,6 @@ sobreButton.place(x=440, y=462)
 titulo.place(x=115, y=100)
 version=tk.Label(tela_principal, text='Versão 1.0.0', font=('Terminal', '10', 'bold italic'))
 version.place(x=5, y=480)
-
+epiButton = tk.Button(tela_principal,font=('System','3') ,text="Cadastrar EPI", height=1, width=20, border=10)
+epiButton.place(x=165, y=300)
 tela_principal.mainloop() #Quero que ela fique rodando
